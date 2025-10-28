a0d74a0b21db0e04a736ab5852a6a08f8b2c8e5a
Tirtho <tirthonag30@gmail.com>
2025-10-28 18:40:55 +0600

    25-60706-1

---
+++ Swap.java
@@ -0,0 +1,22 @@
+public class Swap {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("Before swapping: ");
+        System.out.println("a = " + a + " b = " + b );
+       a = a+b;
+       b = a-b;
+       a = a-b;
+
+        //int temp = a ;
+       // a=b ;
+       //b= temp;
+       System.out.println("After swapping: ");
+       System.out.println("a = " + a + " b = " + b );
+
+    }
+
+
+    
+
+}
